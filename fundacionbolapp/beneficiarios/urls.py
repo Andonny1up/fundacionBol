55b82655d904bc9f64aaa3e3f8a456ee2a501e29
Andoni Barba <andoni1barba@gmail.com>
2023-01-12 21:45:52 -0400

    vistas creadas, prueba de template

--- fundacionbolapp/beneficiarios/urls.py
+++ fundacionbolapp/beneficiarios/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
+app_name = "beneficiarios"
 urlpatterns = [
-    path("",views.index,name="index")
+    path("",views.index,name="index"),
+    #ex: deneficiarios/detalles/5/
+    path("detalles/<int:persona_id>/",views.detallesBeneficiario,name="detallesBeneficiario")
 ]
--- fundacionbolapp/beneficiarios/views.py
+++ fundacionbolapp/beneficiarios/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Persona
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Todu ben")
+    lista_personas = Persona.objects.all()
+    return render(request,"beneficiarios/index.html",{
+        "lista_personas": lista_personas
+    })
+
+
+def detallesBeneficiario(request,persona_id):
+    persona = get_object_or_404(Persona,pk=persona_id)
+    return render(request,"beneficiarios/detalles.html",{
+        "persona": persona
+    })
