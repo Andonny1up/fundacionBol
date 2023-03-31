353c41c2b0f05dd1e358588c0a7c8ba22004f932
Andonny1up <andoni1barba@gmail.com>
2023-03-31 13:47:09 +0000

    inicio rama deploy

--- fundacionbolapp/fundacionbolapp/settings.py
+++ fundacionbolapp/fundacionbolapp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(3nkzgwl2n%-bzp9)o@4@iru(av#nh5t7l7(dnjtxk!aany*q6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -85,10 +85,11 @@ WSGI_APPLICATION = 'fundacionbolapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    
 }
 
 
@@ -127,10 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = []
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ fundacionbolapp/static/beneficiaryapp/js/script.js
@@ -0,0 +1,11 @@
+function confirmDelete() {
+    return confirm('¿Esta seguro de que desea eliminar el registro?')
+}
+
+function confirmBaja() {
+    return confirm('¿Esta seguro de que desea dar de baja a esta persona?')
+}
+
+function confirmFinalized() {
+    return confirm('¿Dese cambiar el estado a finalizado?')
+}
