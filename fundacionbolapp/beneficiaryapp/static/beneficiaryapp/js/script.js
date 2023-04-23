80b1fb2d4f8e8a67700751eabe1c02832b5797d0
Andoni Barba <andoni1barba@gmail.com>
2023-04-23 15:38:32 -0400

    Buscador Global

--- fundacionbolapp/beneficiaryapp/static/beneficiaryapp/js/script.js
+++ fundacionbolapp/beneficiaryapp/static/beneficiaryapp/js/script.js
@@ -1,3 +1,10 @@
+const searchSelect = document.getElementById("id_search_select");
+const options = searchSelect.options;
+const searchInput = document.getElementById("id_search_input");
+const searchBtn = document.getElementById("id_btn_search")
+
+console.log(options)
+
 function confirmDelete() {
     return confirm('¿Esta seguro de que desea eliminar el registro?')
 }
@@ -8,4 +15,19 @@ function confirmBaja() {
 
 function confirmFinalized() {
     return confirm('¿Dese cambiar el estado a finalizado?')
+}
+searchBtn.addEventListener("click",function () {
+    searchInput.value = "";
+    setTimeout(function() {
+        searchInput.focus();
+    }, 700);
+})
+
+searchSelect.addEventListener("focus", function(){
+  searchInput.focus();
+});
+
+
+for (let i = 0; i < options.length; i++) {
+  
 }
